f4e9176c42de4ad4bc646081e8ef87b6a1418180
ElhamDorudian <e.dorudian@gmail.com>
2023-01-25 16:26:11 -0800

    implemented the return button from details page to the list page

--- web/src/components/RepoDetails.tsx
+++ web/src/components/RepoDetails.tsx
@@ -1,6 +1,6 @@
 import { Repo } from '../../../api/src/models/Repo';
-import { Typography } from '@mui/material';
-import { useParams } from 'react-router-dom';
+import { Typography, Button } from '@mui/material';
+import { useParams, useNavigate } from 'react-router-dom';
 import MarkdownContent from './MarkdownContent';
 
 interface Irepo {
@@ -10,15 +10,23 @@ interface Irepo {
 export default function RepoDetails(props: Irepo) {
   const { repoId } = useParams();
   const { filteredRepo } = props;
+  const navigate = useNavigate();
 
-  // const handleHomepage = () => {
-  //   history.push('/');
-  // };
+  const handleHomepage = () => {
+    navigate('/');
+  };
 
   console.log(filteredRepo);
   return (
     <div>
-      <div>
+      <div style={{ margin: '1rem' }}>
+        <Button
+          variant="contained"
+          onClick={handleHomepage}
+          style={{ marginBottom: '1rem' }}
+        >
+          Go to Homepage
+        </Button>
         {filteredRepo &&
           filteredRepo
             .filter((repo) => repo.id.toString() === repoId)
@@ -32,7 +40,6 @@ export default function RepoDetails(props: Irepo) {
                 </Typography>
                 <Typography color="text.secondary">{repo.full_name}</Typography>
                 <MarkdownContent filteredRepo={filteredRepo} repoId={repoId} />
-                {/* <Button onClick={handleHomepage}>Go to Homepage</Button> */}
               </div>
             ))}
       </div>
--- web/src/components/RepoItem.tsx
+++ web/src/components/RepoItem.tsx
@@ -84,59 +84,5 @@ export default function RepoItem(props: IProps) {
         </Grid>
       </Box>
     </>
-    // <Box
-    //   sx={{
-    //     width: '90%',
-    //     margin: 'auto',
-    //     marginTop: '1rem',
-    //   }}
-    // >
-    //   <Grid container={true} rowSpacing={2}>
-    //     {filteredRepo.map((repo: Repo) => (
-    //       <div key={repo.id}>
-    //         <Grid item={true} xs={12} sm={6} md={4}>
-    //           <Item sx={{ margin: 2 }}>
-    //             <Box
-    // flexDirection={{ xs: 'column' }}
-    // sx={{
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   height: 200,
-    // }}
-    //             >
-    //               <Typography variant="h6" component="div">
-    //                 {repo.name}
-    //               </Typography>
-    //               <Typography
-    //                 sx={{ fontSize: 14 }}
-    //                 color="text.secondary"
-    //                 gutterBottom={true}
-    //               >
-    //                 {repo.description}
-    //               </Typography>
-
-    //               <Typography sx={{ mb: 1.5 }} color="text.secondary">
-    //                 {repo.language}
-    //               </Typography>
-    //               <Typography sx={{ mb: 1.5 }} color="text.secondary">
-    //                 {repo.forks_count}
-    //               </Typography>
-    //               <div style={{ display: showDetail ? 'block' : 'none' }}>
-    //                 <RepoDetails filteredRepo={filteredRepo} />
-    //               </div>
-    //               <CardActions>
-    //                 <Button size="small" onClick={handleShowDetails}>
-    //                   <Link to={`/detail/${repo.id.toString()}`}>Detail</Link>
-    //                 </Button>
-    //               </CardActions>
-    //             </Box>
-    //           </Item>
-    //         </Grid>
-    //       </div>
-    //     ))}
-    //   </Grid>
-    // </Box>
   );
 }
-
-// export default repoItem;
